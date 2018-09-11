616b9d1b86205eed292aa2e2a60928d4ee3b8529
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-09-10 21:10:56 -0300

    salario
    Elabore um programa em C que leia as variáveis C e N, respectivamente código e número de horas trabalhadas de um operário. E calcule o salário sabendo-se que ele ganha R$ 10,00 por hora. Quando o número de horas exceder a 50 calcule o excesso de pagamento armazenando-o na variável E, caso contrário zerar tal variável. A hora excedente de trabalho vale R$ 20,00. No final do processamento imprimir o salário total e o salário excedente.

--- salario/main.c
+++ salario/main.c
@@ -4,6 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	float c,n,e,s;
+	
+	printf("Insira o codigo do operario: ");
+	scanf("%f",&c);
+	printf("Insira o numero de horas trabalhadas: ");
+	scanf("%f",&n);
+	
+	if (n > 50) {
+		e = (n - 50)*20;
+		s = 50 * 10;
+		printf("Salario total: %.2f \nSalario excedente: %.2f", s,e);
+	} else {
+		e = 0;
+		s = n * 10;
+		printf("Salario total: R$%.2f \nSalario excedente: R$%.2f", s,e);		
+	}
+	
 	
 	
 	return 0;
