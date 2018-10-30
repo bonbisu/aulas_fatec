283f34ced6a6c2fd7a87615ffd63166456ff5950
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-30 21:24:59 -0200

    correção exercicio 2
    criando uma função

--- Python/PMI09-2.py
+++ Python/PMI09-2.py
@@ -1,6 +1,8 @@
 print('Calculadora km para milhas.')
 km = float(input('Insira a distancia em km: '))
 
-milhas = km * 1.60934
+def conversao (km):
+    milhas = km * 1.60934
+    return milhas
 
-print('A distancia em milhas é : ', milhas)
+print('A distancia em milhas é : ', conversao(km))
