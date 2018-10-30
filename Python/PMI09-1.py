d85fd5a6fe8e0f294275745a57c85e71fc4f5a88
Fernando Costa <31117158+Bonbisu@users.noreply.github.com>
2018-10-30 20:43:04 -0300

    correção da função

--- Python/PMI09-1.py
+++ Python/PMI09-1.py
@@ -2,7 +2,7 @@ km = float(input('Insira a distancia a ser percorrida em km: '))
 consumoMedio = float(input('Insira o consumo médio do veiculo: '))
 
 def consumo (km, consumoMedio):
-    consumoTotal = consumoMedio / km
+    consumoTotal =  km / consumoMedio
     return consumoTotal
 
 print (consumo(km, consumoMedio))
