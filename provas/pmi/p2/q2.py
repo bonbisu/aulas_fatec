ffbcc7fb8192683f5ea965d7b9ee324561ef8f3c
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-11-25 19:08:04 -0200

    persistencia com data incorreta

--- provas/pmi/p2/q2.py
+++ provas/pmi/p2/q2.py
@@ -8,6 +8,7 @@ Armazene estes valore em arquivo CSV, cada valor vai ocupar uma coluna e o arqui
 '''
 
 import csv # para manipular arquivos csv
+import os
 
 '''
 Atualização do script para capturar a data atual da inserção dos dados
@@ -20,11 +21,28 @@ now = str(now.day) + '-' + str(now.month) + '-' + str(now.year) # fomatado para 
 csvFile = open('temperaturas.csv', 'a')
 # cria um objeto writer atribuido a uma variavel
 temp = csv.writer(csvFile)
+dia = 0
+mes = 0
+ano = 0
+while (dia > 31) or (dia < 1):
+    dia = int(input("Insira a dia da medição: "))
+    if (dia > 31) or (dia < 1):
+        print('Valor para DIA incorreto. ex: 01 ')
+        os.system("cls")
 
-data = input("Insira a dia da medição: ")
-data = data + "-" + input("Insira a mês da medição: ")
-data = data + "-" + input("Insira a ano da medição: ")
+while (mes > 12) or (mes < 1):
+    mes = int(input("Insira a mês da medição: "))
+    if (mes > 12) or (mes < 1):
+        print('Valor para MES incorreto. ex: 11')
+        os.system("cls")
 
+while len(str(ano)) != 4:
+    ano = int(input("Insira a ano da medição: "))
+    if len(str(ano)) != 4:
+        print('Valor para ANO incorreto. ex: 2018')
+        os.system("cls")
+
+data = str(dia).zfill(2) + '-' + str(mes).zfill(2) + '-' + str(ano) # zfill completa com zeros a esquerda quando necessário
 min = float(input('Insira a temperatura minima : '))
 max = float(input('Insira a temperatura maxima : '))
 
@@ -33,6 +51,7 @@ if min > max:
     t = min
     min = max
     max = t
+
 # gravar no csv
 temp.writerow([data, min, max])
 # fechar arquivo
