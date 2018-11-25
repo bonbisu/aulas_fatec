6ec599f30619013abb559ad9d3a0090ee68a391e
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-11-25 18:04:17 -0200

    P2 - questão 2

---
+++ provas/pmi/p2/q2.py
@@ -0,0 +1,32 @@
+'''
+Crie um script em python que faça a entradas de 3 valores:
+
+- Data
+- Temperatura minima
+- Temperatura máxima
+Armazene estes valore em arquivo CSV, cada valor vai ocupar uma coluna e o arquivo deve ser atualizado a cada execução do script (3,0)
+'''
+
+import csv # para manipular arquivos csv
+
+'''
+Atualização do script para capturar a data atual da inserção dos dados
+from datetime import datetime # para extrair a data atual
+now = (datetime.now() ) # data e hora atual
+now = str(now.day) + '-' + str(now.month) + '-' + str(now.year) # fomatado para o csv
+'''
+
+# abrir o arquivo
+csvFile = open('temperaturas.csv', 'a')
+# cria um objeto writer atribuido a uma variavel
+temp = csv.writer(csvFile)
+
+data = input("Insira a dia da medição: ")
+data = data + "-" + input("Insira a mês da medição: ")
+data = data + "-" + input("Insira a ano da medição: ")
+
+min = float(input('Insira a temperatura minima : '))
+max = float(input('Insira a temperatura maxima : '))
+
+temp.writerow([data, min, max])
+csvFile.close()
