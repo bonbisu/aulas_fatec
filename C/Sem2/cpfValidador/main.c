503a81922257d308f884a0f907b712c7b89faa67
bonbisu <fernando.costa30@fatec.sp.gov.br>
2019-03-09 11:27:48 -0300

    :rotating_light: refatctor(validator): add functions to validate

--- C/Sem2/cpfValidador/main.c
+++ C/Sem2/cpfValidador/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h> // for string manipulation (e.g. strlen() )
+
+bool strIsNumber(char c)
+{
+  if (c >= '0' && c <= '9') // ao subtrair um numero como string é utilizado seu código ascii
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int convertNumber(char c)
+{
+  // o codigo ascii de '0' é igual 48.
+  // Subtraindo 48 de 48 se encontra o inteiro 0. O mesmo se aplica para os demais numero/strings.
+  // e.g. '3' = 51 => 51 - 48 = 3
+  return c - '0';
+}
 
 bool verificaDigito(int cpf[], int multiplicador[], int N)
 {
@@ -20,17 +41,37 @@ bool verificaDigito(int cpf[], int multiplicador[], int N)
     return false;
 }
 
+void converter(char string[], int vet[])
+{
+  int i = strlen(string) - 1; // indice do ultimo char
+  int j = 10;                 // indice do vetor numerico
+  while (i >= 0)
+  {
+    if (strIsNumber(string[i]))
+    {
+      vet[j] = convertNumber(string[i]);
+      j--;
+    }
+    i--;
+  }
+  while (j >= 0)
+  {
+    vet[j] = 0;
+    j--;
+  }
+}
+
 int main()
 {
   int cpf[11];
   int m1[9] = {10, 9, 8, 7, 6, 5, 4, 3, 2};
   int m2[10] = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
   int i;
-  printf("Entre com os 11 digitos do CPF:");
-  for (i = 0; i < 11; i++)
-  {
-    scanf("%d", &cpf[i]);
-  }
+  char cpfstring[20];
+
+  printf("Entre com o CPF: ");
+  gets(cpfstring);
+  converter(cpfstring, cpf);
 
   // verifica CPF
 
